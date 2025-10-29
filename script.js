e01eb1a321fab418f518efbd8eeda9b444dbdb7a
Malcolm Harris <harrismalcolm1@gmail.com>
2025-10-29 18:19:25 -0400

    updated css and js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 256; i++) {
     container.innerHTML += '<div class="box"></div>';
 }
+
+let boxes = document.querySelectorAll(".box");
+boxes.forEach( box => {
+    box.addEventListener('mouseover', function(e) {
+        e.target.style.background = "blue";
+    });
+});
